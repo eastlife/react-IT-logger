5772a19fa79d0316042dfa39e2e4ab1d90dede50
eastlife <dengbx1212@163.com>
2020-07-12 16:32:04 -0700

    delete technician

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -1,4 +1,4 @@
-import {GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR, GET_LOGS, LOGS_ERROR} from "./types";
+import {GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR} from "./types";
 
 // Get techs from server
 export const getTechs = () => async dispatch => {
@@ -37,6 +37,27 @@ export const addTech = (tech) => async dispatch => {
         dispatch({
             type: ADD_TECH,
             payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: TECHS_ERROR,
+            payload: err.response.statusText
+        });
+    }
+}
+
+// Delete tech from server
+export const deleteTech = (id) => async dispatch => {
+    try {
+        setLoading();
+
+        await fetch(`/techs/${id}`, {
+            method: 'DELETE'
+        });
+
+        dispatch({
+            type: DELETE_TECH,
+            payload: id
         });
     } catch (err) {
         dispatch({
